0bc1bfe490deee2619b9cf991893793b73b8334c
Firdavs39 <legogosalat@gmail.com>
2025-05-24 16:12:59 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 ДОБАВЬТЕ ЭТУ СТРОКУ
+  base: "/vremya-iz-uzbekistan/", // Укажите здесь название вашего репозитория!
   server: {
     host: "::",
     port: 8080,
